2fdc6445eebae5bcf72ecb62aa8c170ea35772cc
Alexis Mendoza <afmendozaf@uce.edu.ec>
2025-02-11 00:44:48 -0500

    feat(model): 🎉 add email validation and unique constraint to user model

--- register-microservice/models/User.js
+++ register-microservice/models/User.js
@@ -2,15 +2,27 @@ const { Sequelize, DataTypes } = require('sequelize');
 const sequelize = require('../db');
 
 const User = sequelize.define('User', {
-    username: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-    },
-    password: {
-        type: DataTypes.STRING,
-        allowNull: false,
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,  // Garantiza que el nombre de usuario sea único
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,  // Garantiza que el correo electrónico sea único
+    validate: {
+      isEmail: {
+        msg: "Debe ser un correo electrónico válido"
+      },
     }
+  },
+}, {
+  timestamps: true, // Esto automáticamente agrega createdAt y updatedAt
 });
 
 module.exports = User;
