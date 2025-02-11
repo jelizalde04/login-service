7c940998f407118957abddf0160d87cf3e110fcb
Johana Elizalde <njelizalde@uce.edu.ec>
2025-02-10 22:22:36 -0500

    feat(models): 🛠 define User model with id, username, and password fields

---
+++ login-microservice/models/User.js
@@ -0,0 +1,22 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../db');
+
+// Definir el modelo de Usuario
+const User = sequelize.define('User', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+});
+
+module.exports = User;
