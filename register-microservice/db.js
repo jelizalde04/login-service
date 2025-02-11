cf69db38377e9e90133a9d84349dfa1275c7daaa
Alexis Mendoza <afmendozaf@uce.edu.ec>
2025-02-11 00:44:23 -0500

    chore(server): 🚀 start server on specified port

--- register-microservice/db.js
+++ register-microservice/db.js
@@ -1,14 +1,24 @@
+// db.js
 const { Sequelize } = require('sequelize');
-require('dotenv').config();  // Cargar las variables de entorno
+require('dotenv').config();
 
-// Configurar la conexión a la base de datos en RDS
 const sequelize = new Sequelize({
-    dialect: 'postgres',
-    host: process.env.DB_HOST,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    logging: false,  // Deshabilitar el log para mayor rendimiento
+  dialect: 'postgres',
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  logging: false,
 });
+
+// Verificar la conexión y sincronizar las tablas
+sequelize.authenticate()
+  .then(() => {
+    console.log('Conexión exitosa a la base de datos');
+    sequelize.sync({ force: false });  // 'force: false' para no borrar las tablas existentes
+  })
+  .catch((err) => {
+    console.error('No se pudo conectar a la base de datos:', err);
+  });
 
 module.exports = sequelize;
